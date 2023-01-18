e64cc1f48841bc510d2b5ba6f79d4700eccc77fc
Sergei Gots <Serge.Gots@gmail.com>
2023-01-18 23:12:14 +0300

    L.52 "One-To-Many". session.save(Object object) added.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.model.Item;
 import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -11,15 +12,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration().
+                addAnnotatedClass(Person.class).
+                addAnnotatedClass(Item.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
         try {
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 1);
-            System.out.println(person);
+            Person person = session.get(Person.class, 2);
+
+            Item item = new Item("Item from Hibernate", person);
+
+            person.getItems().add(item);  //doesn't affect DB, affects local cash
+
+            session.save(item);
+
             session.getTransaction().commit();
         }
         finally {
---
+++ src/main/java/org/example/model/Item.java
@@ -0,0 +1,61 @@
+package org.example.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Item")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person owner;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Person owner) {
+        this.itemName = itemName;
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", itemName='" + itemName + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+
+}
